acd22bdb77ec00fd4abd219a959fd83e7634732c
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-03-03 10:41:00 -0300

    Initial commit

---
+++ src/test/java/com/avalicao3/states/StatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avalicao3.states;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
